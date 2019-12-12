901ad380578b3778f20a4fca6c80f26e6d187cb4
Adam Callanan <a.callanan@sbcglobal.net>
2019-12-11 21:00:48 -0600

    allow local env

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,19 @@
-module.exports = {
-  mongoURI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0-laibl.mongodb.net/test?retryWrites=true&w=majority`
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    mongoURI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0-laibl.mongodb.net/test?retryWrites=true&w=majority`
+  };
+} else {
+  try {
+    const {MONGO_USER, MONGO_PASS} = require("../local.env");
+    module.exports = {
+      mongoURI: `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@cluster0-laibl.mongodb.net/test?retryWrites=true&w=majority`
+    };
+  }
+  catch (e) {
+    if (e instanceof Error && e.code === "MODULE_NOT_FOUND")
+      console.log("No db setup");
+    else
+      throw e;
+  }
+}
+
--- config/secret.config.js
+++ config/secret.config.js
@@ -1,3 +1,18 @@
-module.exports = {
-  secret: process.env.SECRET
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    secret: process.env.SECRET
+  };
+} else {
+  try {
+    const {SECRET} = require("../local.env");
+    module.exports = {
+      secret: localSecret
+    };
+  }
+  catch (e) {
+    if (e instanceof Error && e.code === "MODULE_NOT_FOUND")
+      console.log("No secret setup");
+    else
+      throw e;
+  }
+}
