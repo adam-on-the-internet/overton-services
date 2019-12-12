0e38885c2c7223cd7758b6b686b2339abd7fcfd0
Adam Callanan <a.callanan@sbcglobal.net>
2019-12-11 20:52:58 -0600

    configuring application

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+// bring in controllers
+const showController = require('./controllers/show.controller');
+
 const app = express();
 
+// DB setup
+const db = require('./config/database.config');
+mongoose.connect(db.mongoURI, {
+  useNewUrlParser: true,
+})
+.then(() => {
+  logInfo("mongodb connected...");
+})
+.catch((err) => {
+  logError(err);
+});
+
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', ['Authorization', 'Content-Type']);
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  next();
+}
+app.use(allowCrossDomain);
+
+// use controllers
+app.use('/show', showController);
+
+// naked route
 app.get('/', (req, res) => {
   res.send('overton services running on heroku');
 });
---
+++ config/database.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0-laibl.mongodb.net/test?retryWrites=true&w=majority`
+};
