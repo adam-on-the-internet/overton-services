467db0031663be0c2b1f6c3a90c556eb364ea130
Adam Callanan <a.callanan@sbcglobal.net>
2019-12-11 20:25:56 -0600

    test heroku push

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Overton services works!');
+  res.send('overton services running on heroku');
 });
 
 const port = process.env.PORT || 5000;
