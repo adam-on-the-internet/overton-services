eb6b8b589b4ff3968f61eb89047f46d589b80bd0
Adam Callanan <a.callanan@sbcglobal.net>
2019-12-15 15:21:29 -0600

    added get all text endpoint

--- controllers/text.controller.js
+++ controllers/text.controller.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const textController = express.Router();
+const { jwtAuthenticated } = require('../utilities/auth.util');
+const {
+  getAllText
+} = require("../managers/text.manager");
 
 textController.get('/', (req, res) => {
-  res.send('text controller');
+  getAllText()
+    .then((text) => {
+      res.send({
+        text: text
+      });
+    })
+    .catch((err) => {
+      res.statusCode = 500;
+      res.send(err);
+    });
 });
 
 module.exports = textController;
