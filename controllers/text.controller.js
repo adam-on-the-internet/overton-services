a547710aa7c388f8e58458edcd7312ebfb57e732
Adam Callanan <a.callanan@sbcglobal.net>
2019-12-15 16:05:17 -0600

    CRUD for text

--- controllers/text.controller.js
+++ controllers/text.controller.js
@@ -2,7 +2,10 @@ const express = require('express');
 const textController = express.Router();
 const { jwtAuthenticated } = require('../utilities/auth.util');
 const {
-  getAllText
+  getAllText,
+  addText,
+  updateText,
+  deleteOneText
 } = require("../managers/text.manager");
 
 textController.get('/', (req, res) => {
@@ -11,6 +14,42 @@ textController.get('/', (req, res) => {
       res.send({
         text: text
       });
+    })
+    .catch((err) => {
+      res.statusCode = 500;
+      res.send(err);
+    });
+});
+
+textController.post('/', jwtAuthenticated, (req, res) => {
+  const text = req.body;
+  addText(text)
+    .then((text) => {
+      res.send(text);
+    })
+    .catch((err) => {
+      res.statusCode = 500;
+      res.send(err);
+    });
+});
+
+textController.delete('/:id', jwtAuthenticated, (req, res) => {
+  const id = req.params.id;
+  deleteOneText(id)
+    .then((response) => {
+      res.send(response);
+    })
+    .catch((err) => {
+      res.statusCode = 500;
+      res.send(err);
+    });
+});
+
+textController.put('/', jwtAuthenticated, (req, res) => {
+  const text = req.body;
+  updateText(text)
+    .then((response) => {
+      res.send(response);
     })
     .catch((err) => {
       res.statusCode = 500;
--- managers/text.manager.js
+++ managers/text.manager.js
@@ -11,6 +11,77 @@ function getAllText() {
   });
 }
 
-module.exports = {
-  getAllText
+function addText(text) {
+  return new Promise((resolve, reject) => {
+    const errors = checkForTextErrors(text);
+    if (errors.length > 0) {
+      reject(errors);
+    }
+    else {
+      new Text({
+        areaName: text.areaName,
+        textContent: text.textContent
+      })
+        .save()
+        .then((resText) => {
+          resolve(resText);
+        });
+    }
+  });
 }
+
+function deleteOneText(id) {
+  return new Promise((resolve, reject) => {
+    Text.deleteOne({
+      _id: id
+    })
+      .then(() => {
+        resolve({
+          message: `Text with given id no longer exists`
+        });
+      });
+  });
+}
+
+function updateText(text) {
+  return new Promise((resolve, reject) => {
+    const id = text._id;
+
+    Text.findOne({
+      _id: id
+    })
+      .then((foundText) => {
+        if (!foundText) {
+          reject({
+            message: `Failed to find text`
+          });
+        }
+
+        foundText.areaName = text.areaName;
+        foundText.textContent = text.textContent;
+
+        foundText.save()
+          .then((editedText) => {
+            resolve(editedText);
+          });
+      });
+  });
+}
+
+module.exports = {
+  getAllText,
+  addText,
+  updateText,
+  deleteOneText
+}
+
+const checkForTextErrors = ((text) => {
+  let errors = [];
+  if (!text.areaName) {
+    errors.push({ text: 'Please add an area name' });
+  }
+  if (!text.textContent) {
+    errors.push({ text: 'Please add text content' });
+  }
+  return errors;
+});
