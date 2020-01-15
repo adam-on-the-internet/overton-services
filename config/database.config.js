c85af39b9b9a95efe1f99832cb2c8fd4a9db8acb
Adam Callanan <a.callanan@sbcglobal.net>
2020-01-14 21:51:50 -0600

    mongo cluster var

--- config/database.config.js
+++ config/database.config.js
@@ -6,11 +6,13 @@ let mongoPass;
 if (runningProd()) {
   mongoUser = process.env.MONGO_USER;
   mongoPass = process.env.MONGO_PASS;
+  mongoCluster = process.env.MONGO_CLUSTER;
 } else {
   try {
-    const { MONGO_USER, MONGO_PASS } = require("../local.env");
+    const { MONGO_USER, MONGO_PASS, MONGO_CLUSTER } = require("../local.env");
     mongoUser = MONGO_USER;
     mongoPass = MONGO_PASS;
+    mongoCluster = MONGO_CLUSTER;
   }
   catch (e) {
     if (e instanceof Error && e.code === "MODULE_NOT_FOUND")
@@ -21,5 +23,5 @@ if (runningProd()) {
 }
 
 module.exports = {
-  mongoURI: `mongodb+srv://${mongoUser}:${mongoPass}@cluster0-laibl.mongodb.net/test?retryWrites=true&w=majority`
+  mongoURI: `mongodb+srv://${mongoUser}:${mongoPass}@${mongoCluster}/test?retryWrites=true&w=majority`
 };
